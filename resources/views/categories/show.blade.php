a81fc83827c7b6d02f65fc1c41c63f6645bc2155
Your Name <ditrihbajic@tippnet.rs>
2017-07-21 13:18:44 +0200

    Gallery

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -1,20 +1,38 @@
 @extends('layout')
 
 @section ('content')
-	<div class="starter-template">
-		<h1>Listing products from category '{{ $category->name }}'</h1>
 
-		<ul>
-		<table>
-			@foreach ($category->products as $product)
-				<tr><td><li>
-				
-					<a href="{{ route('product', [$product->id]) }}">
-						<img src="{{asset($product->picture)}}"><br>{{ $product->name }}
-					</a>
-				</li></td></tr>
-			@endforeach
-			</table>
-		</ul>
-	</div>
+<style>
+div.gallery {
+    margin: 5px;
+    border: 0px solid #ccc;
+    float: left;
+    width: 180px;
+}
+
+div.gallery:hover {
+    border: 1px solid #777;
+}
+
+div.gallery img {
+    width: 100%;
+    height: auto;
+}
+
+div.desc {
+    padding: 15px;
+    text-align: center;
+}
+</style>
+	<div class="starter-template">
+		<!--<h1>Listing products from category '{{ $category->name }}'</h1>-->
+@foreach ($category->products as $product)
+<div class="gallery">
+<a target="" href="{{ route('product', [$product->id]) }}">
+<img src="{{asset($product->picture)}}" alt="{{asset($product->picture)}}" width="300" height="200">
+  </a>
+<div class="desc">{{ $product->name }} Price: {{ $product->price }} </div>
+</div>
+@endforeach
+<div>
 @endsection
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -2,8 +2,8 @@
 
 @section ('content')
 	<div class="starter-template">
-		<h1>Listing product '{{ $product->name }}'</h1>
-<img src="{{asset($product->picture)}}">
-		This product belongs to '{{ $product->category->name }}'
+		<!--<h1>Listing product '{{ $product->name }}'</h1>-->
+<img src="{{asset($product->picture)}}"><br>{{ $product->name }} Price: {{ $product->price }}
+		<!--This product belongs to '{{ $product->category->name }}'-->
 	</div>
 @endsection
