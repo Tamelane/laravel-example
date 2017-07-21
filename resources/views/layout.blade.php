d0a0309044ef6099996969c75409e1a48186fe31
Your Name <ditrihbajic@tippnet.rs>
2017-07-21 11:36:05 +0200

    navbar

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,6 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<!--
+<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no">
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-2">
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1250">
+-->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+<meta name="keywords" content="ribolov, ribolovacka-oprema, stapovi, pecanje, mamci, varalice, ">
+<meta name="description" content="Ribolovacka oprema za pecarose, oprema za ribolov, stapovi za ribolov, masinice za ribolov,feeder oprema i carp oprema">
+<meta name="author" content="Branislav Skenderovic, Ditrih Bajic">
+<meta name="robots" content="index, follow">
+<meta name="googlebot" content="NOODP">
+<meta http-equiv="Content-language" content="sr">
+
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="css/animate.css">
+
+<meta property="og:type"   content="article">
+<meta property="og:url"    content="http://ribolov.webutu.com">
+<meta property="og:title"  content="Ribolovacka oprema">
+<meta property="og:description" content="Najpovoljnija ribolovacka oprema!">
+<meta property="og:image"  content="http://ribolov.webutu.com/photos/logo.png">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -14,6 +41,7 @@
     <link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet">
 
     <link href="{{ asset('/css/starter-template.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/style.css') }}" rel="stylesheet">
   </head>
 
   <body>
--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-inverse navbar-fixed-top">
+<!--<nav class="navbar navbar-inverse navbar-fixed-top">
   <div class="container">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -21,3 +21,74 @@
     </div>
   </div>
 </nav>
+-->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand glyphicon glyphicon-home" href="{{ route('home') }}"></a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav">
+            <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                    <img src="{{asset('/images/stap.png')}}" alt="ikonica za stap" style="height: 20px; width:20px"/> Štapovi <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="{{ route('filter_category_by_name', ['feeder']) }}">Feeder</a></li>
+                    <li><a href="{{ route('filter_category_by_name', ['match']) }}">Match</a></li>
+                    <li><a href="{{ route('filter_category_by_name', ['picker']) }}">Picker</a></li>
+                </ul>
+            </li>
+            <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                    <img src="{{asset('/images/masinica.png')}}" alt="ikonica za masinicu" style="height: 20px; width:20px"/> Mašinice <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="{{ route('filter_category_by_name', ['falcon']) }}">Falcon</a></li>
+                    <li><a href="{{ route('filter_category_by_name', ['shimano']) }}">Shimano</a></li>
+                    <li><a href="{{ route('filter_category_by_name', ['spro']) }}">Spro</a></li>
+                </ul>
+            </li>
+            <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                    <img src="{{asset('/images/varalica.png')}}" alt="ikonica za varalicu" style="height: 20px; width:20px"/> Varalice <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="{{ route('filter_category_by_name', ['povrsinske']) }}">Površinske</a></li>
+                    <li><a href="{{ route('filter_category_by_name', ['specijalne']) }}">Specijalne</a></li>
+                </ul>
+            </li>
+            <?php
+/*            require ("functions_def.php");
+            if(isset($_SESSION['id_user']) AND is_numeric($_SESSION['id_user'])){
+
+            echo '<li><a href="logout.php">Logout</a></li>';
+            }*/?>
+            
+        </ul>
+        <div class="col-sm-3 col-md-3">
+            <form class="navbar-form" role="search" action="{{ route('filter_product_by_name', ['FalconForzaESR-10']) }}" method="get">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Pretraga" name="name">
+                    <div class="input-group-btn">
+                        <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+
+
+
+    </div><!-- /.navbar-collapse -->
+</nav>
+    <div id="header">
+        <img src="{{asset('/images/logo2.png')}}" alt="logo"/>
+        <h3 class="animated fadeInLeftBig">Sve za pecanje na jednom mestu!</h3><br>
+
+
+
+    </div>
+
